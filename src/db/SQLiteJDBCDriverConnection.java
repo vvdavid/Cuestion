9a7c03cb1efd57577a6f32759388d159cc5ec934
David Vazquez <vvdavided@gmail.com>
2018-07-26 13:37:50 -0500

    Agrega archivo de conexión a bd

---
+++ src/db/SQLiteJDBCDriverConnection.java
@@ -0,0 +1,40 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author David Vazquez
+ */
+public class SQLiteJDBCDriverConnection {
+
+    public static final String DB_NAME = "db.db";
+    public static final String DB_URL = "jdbc:sqlite:" + DB_NAME;
+
+    public static Connection connect() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(DB_URL);
+        } catch (SQLException e) {
+            System.out.println(e);
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex);
+            }
+        }
+        return conn;
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println(connect());
+    }
+}
