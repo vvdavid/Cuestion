d90b37395a0d6c4f7d9e658174462d594ef61235
David Vazquez <vvdavided@gmail.com>
2018-07-26 02:48:10 -0500

    Elimina comentario

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -10,7 +10,6 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-//        com.alee.laf.WebLookAndFeel.install();
         Start.INSTANCE.setVisible(true);
     }
 
