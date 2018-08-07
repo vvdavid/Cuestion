f60d18213a9d4580ae2ea65823d0ad11d82b323f
David Vazquez <vvdavided@gmail.com>
2018-08-07 09:44:40 -0500

    Agrega métodos auxiliares

--- src/db/DBConnection.java
+++ src/db/DBConnection.java
@@ -4,7 +4,11 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
@@ -33,8 +37,12 @@ public class DBConnection {
         return conn;
     }
 
-    private static void closeStuff(ResultSet resultSet, PreparedStatement preparedStatement, Connection connection) throws SQLException {
+    public static void closeStuff(ResultSet resultSet, PreparedStatement preparedStatement, Connection connection) throws SQLException {
         resultSet.close();
+        preparedStatement.close();
+        connection.close();
+    }
+    public static void closeStuff(PreparedStatement preparedStatement, Connection connection) throws SQLException {
         preparedStatement.close();
         connection.close();
     }
@@ -44,5 +52,27 @@ public class DBConnection {
      */
     public static void main(String[] args) {
         System.out.println(getConnection());
+    }
+
+    public static DefaultTableModel resultSetToDefaultTableModel(ResultSet source) {
+        DefaultTableModel modelo = new DefaultTableModel();
+        try {
+            ResultSetMetaData meta = source.getMetaData();
+            for (int i = 0; i < meta.getColumnCount(); i++) {
+                modelo.addColumn(meta.getColumnLabel(i + 1));
+            }
+            while (source.next()) {
+                Object[] fila = new Object[meta.getColumnCount()];
+                for (int i = 0; i < meta.getColumnCount(); i++) {
+                    fila[i] = source.getObject(i + 1);
+                }
+                modelo.addRow(fila);
+            }
+            source.close();
+            return modelo;
+        } catch (SQLException ex) {
+            System.err.println(ex);
+            return null;
+        }
     }
 }
